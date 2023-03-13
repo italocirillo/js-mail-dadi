25c1544537090aceb8e8035917af40c541aac3a0
italocirillo <italocirillo@gmail.com>
2023-03-13 19:26:57 +0100

    sezione email fatta

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-console.log("js collegato");
+// ESERCIZIO EMAIL
+
+//BOTTONE INVIA PIGIATO
+const bottoneInvia= document.getElementById("invia");
+bottoneInvia.addEventListener("click", function(){
+    // INIZIALIZZO VARIABILI
+    const listaEmail = ["pincopallo@gmail.com","lucalaurenti@gmail.com","francorossi@gmail.com","tiziotiziani@gmail.com","caiosempronio@gmail.com"];
+    let verificaEmail="NON È NELLA LISTA";
+    let emailUtente;
+
+    //INPUT
+    emailUtente=document.getElementById("email");
+
+    //LOGICA
+    for(let i=0;i<listaEmail.length;i++){
+        if(listaEmail[i] === emailUtente.value){
+            verificaEmail="È NELLA LISTA";
+        }
+    }
+
+    //OUTPUT
+    document.getElementById("stato-email").innerHTML=emailUtente.value + " " + verificaEmail;
+
+    //Ripulisco gli output
+    emailUtente.value="";
+
+})
+
+
+
+
+
+
+
+
+
+
